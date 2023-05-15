18d6c52b9cc40d390fe7b26739b022dc86866575
TheBestByte <dom_jackson_92@hotmail.com>
2023-05-15 20:43:17 +1000

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/bin2win', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/bin2win',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
